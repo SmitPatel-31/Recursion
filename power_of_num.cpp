46ed14e2ee9c6a34d70a0047b448598840895879
SmitPatel-31 <patel.smit3131@gmail.com>
2021-01-05 10:31:02 +0530

    Update power_of_num.cpp

--- power_of_num.cpp
+++ power_of_num.cpp
@@ -1,3 +1,21 @@
+
+// Input format :
+// Two integers x and n (separated by space)
+// Output Format :
+// x^n (i.e. x raise to the power n)
+// Constraints:
+// 0 <= x <= 8 
+// 0 <= n <= 9
+// Sample Input 1 :
+//  3 4
+// Sample Output 1 :
+// 81
+// Sample Input 2 :
+//  2 5
+// Sample Output 2 :
+// 32
+
+
 #include<iostream>
 using namespace std;
 
